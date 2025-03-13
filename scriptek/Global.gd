f04757b8f649bbe265d3954b4573b4cdba82121c
Niesz Martin <60209474+Martynn25@users.noreply.github.com>
2025-03-13 15:28:46 +0100

    Dialogue dinamic input display

--- scriptek/Global.gd
+++ scriptek/Global.gd
@@ -1,8 +1,10 @@
 extends Node
 @export var IsInDialogue = false
-
+var Interact : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Interact = InputMap.action_get_events("Interact")[0].as_text().rstrip(" (Physical)")
+	print(Interact)
 	pass # Replace with function body.
 
 
